b54f205d556c2d4932b07454e6d1f021142f078b
GorNalbandyan <gor.ar.nalb@gmail.com>
2024-06-20 23:28:18 +0400

    updated query

--- SQLQueries/StudyGroupQueries.sql
+++ SQLQueries/StudyGroupQueries.sql
@@ -1,10 +1,9 @@
 SELECT sg.*
 FROM StudyGroups sg
-JOIN Users u ON sg.UserId = u.UserId
+JOIN StudyGroupUsers sgu ON sg.StudyGroupId = sgu.StudyGroupId
+JOIN Users u ON sgu.UserId = u.UserId
 WHERE u.Name LIKE 'M%'
 ORDER BY sg.CreateDate;
 
---The query assumes the presence of both UserId and Name columns in the Users table. We make this assumption to enable the join operation between 
---the StudyGroups and Users tables based on the UserId column, and to filter the results based on the Name column. This assumption allows us to retrieve 
---study groups that have at least one associated user whose name starts with 'M'. While we acknowledge that specific database schema may vary, 
---this assumption is made based on typical conventions for representing users in relational databases.
+--StudyGroupUsers (sgu): This table acts as the junction table that connects StudyGroups and Users. 
+--Assumingly, it  contains two columns: StudyGroupId and UserId establishing the many-to-many relationship.
